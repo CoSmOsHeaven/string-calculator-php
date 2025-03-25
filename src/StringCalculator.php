55b53b1b51c15fd8be162bae6a0e8fa366be8e14
Ruben Zubasti <rubenix2.0@gmail.com>
2025-03-25 12:33:44 +0100

    [] - Test new line

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,7 +4,17 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    public function add(string $numbers): int{
-        return array_sum(str_split($numbers));
+    public function add(string $numbers) : int{
+        $numbers = str_replace("\n", ',', $numbers);
+
+        if(str_contains($numbers, ',')) {
+            return array_sum(explode(',', $numbers));
+        }
+
+        if(empty($numbers)) {
+            return 0;
+        }
+
+        return (int) $numbers;
     }
 }
